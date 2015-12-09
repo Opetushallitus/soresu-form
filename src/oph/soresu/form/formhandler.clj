69d52ddeee6cde6d019a94fa3bd05401e66ed8e0
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-12-09 09:48:32 +0200

    Tolerate empty field type selections for koodisto fields

--- src/oph/soresu/form/formhandler.clj
+++ src/oph/soresu/form/formhandler.clj
@@ -2,10 +2,16 @@
   (require [oph.soresu.form.formutil :as formutil]
            [oph.soresu.common.koodisto :as koodisto]))
 
+(def default-koodisto-field-type "dropdown")
+
 (defn- process-koodisto-field [node operation]
   (if (= "koodistoField" (:fieldType node))
       (operation node)
       node))
+
+(defn- resolve-field-type [koodisto-field]
+  (or (-> koodisto-field :params :inputType)
+      default-koodisto-field-type))
 
 (defn- add-koodisto-options [koodisto-field-node]
   (let [koodisto-params (-> koodisto-field-node :params :koodisto)
@@ -14,7 +20,7 @@
     (assoc koodisto-field-node :options (koodisto/get-koodi-options koodisto-uri version))))
 
 (defn- set-field-type-from-params [koodisto-field-node]
-  (assoc koodisto-field-node :fieldType (-> koodisto-field-node :params :inputType)))
+  (assoc koodisto-field-node :fieldType (resolve-field-type koodisto-field-node)))
 
 (defn- transform-koodisto-fields [node-operation form]
   (formutil/transform-form-content form #(process-koodisto-field % node-operation)))
