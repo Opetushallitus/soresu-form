4e7c4d05d8a9220a92e29bacc0d2b55a5ef22d33
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-02-22 11:31:15 +0200

    Validate email address domain part more strictly

--- web/test/SyntaxValidatorTest.js
+++ web/test/SyntaxValidatorTest.js
@@ -24,8 +24,16 @@ describe('Syntax validator', function() {
   it('can validate basic email', function() {
     expect(SyntaxValidator.validateEmail("valid.email@example.com")).to.equal(undefined)
   })
+  it('can validate email with longer domain part', function() {
+    expect(SyntaxValidator.validateEmail("valid.email@my-example.DOT.com")).to.equal(undefined)
+  })
   it('notices invalid email with only one domain part', function() {
     const result = SyntaxValidator.validateEmail("invalid.email@example")
+    expect(result).to.have.property("error")
+    expect(result.error).to.equal("email")
+  })
+  it('notices invalid email with , in domain part', function() {
+    const result = SyntaxValidator.validateEmail("invalid.email@example.,com")
     expect(result).to.have.property("error")
     expect(result.error).to.equal("email")
   })
