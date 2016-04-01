0e8048683948b6c2974d08a63092a81dbcef1a5f
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-01 14:02:18 +0300

    Webpack soresu

--- soresu.js
+++ soresu.js
@@ -1,10 +1,19 @@
-const form = require.context("./web/form", /*useSubdirectories*/ true)
-//const component = require.context("./web/form/component", /*useSubdirectories*/ true)
-//const edit = require.context("./web/form/edit", /*useSubdirectories*/ true)
-//const img = require.context("./web/form/img", /*useSubdirectories*/ true)
-//const preview = require.context("./web/form/preview", /*useSubdirectories*/ true)
-//const style = require.context("./web/form/style", /*useSubdirectories*/ true)
+const form = require.context("./web/form", /*useSubdirectories*/ false)
+const component = require.context("./web/form/component", /*useSubdirectories*/ true)
+const edit = require.context("./web/form/edit", /*useSubdirectories*/ true)
+const img = require.context("./web/form/img", /*useSubdirectories*/ true)
+const preview = require.context("./web/form/preview", /*useSubdirectories*/ true)
+const style = require.context("./web/form/style", /*useSubdirectories*/ true)
 
-module.exports =
-  form.keys().map(form)
+const f = x => x.keys().map(x)
 
+const exports = {
+  form: f(form),
+  component: f(component),
+  edit: f(edit),
+  img: f(img),
+  preview: f(preview),
+  style: f(style)
+}
+
+module.exports = exports
