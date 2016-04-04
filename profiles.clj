73c91929428f9be350e27891331d8d6a750c843c
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-04 16:35:01 +0300

    Add oph repositories in a custom profile

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,10 @@
 {
+ :repo {:repositories [["releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
+                                   :sign-releases false
+                                   :snapshots false}]
+                      ["snapshots"      {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"}]
+                      ["ext-snapshots"  {:url "https://artifactory.oph.ware.fi/artifactory/ext-snapshot-local"}]
+                      ["Laughing Panda" {:url "http://maven.laughingpanda.org/maven2"
+                                         :snapshots false}]]}
  :ci {:env {}}
 }
