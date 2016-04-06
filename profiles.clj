77d353ba8b8324bc73b9a8d2188c144acf7d45c6
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-06 16:29:41 +0300

    rename profile

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,10 @@
 {
- :repo {:repositories [["releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
-                                   :sign-releases false
-                                   :snapshots false}]
-                      ["snapshots"      {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"}]
+ :soresu-lib {:repositories [["releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
+                                          :sign-releases false
+                                          :snapshots false
+                                          :creds :gpg}]
+                             ["snapshots"      {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"
+                                                :creds :gpg}]
                       ["ext-snapshots"  {:url "https://artifactory.oph.ware.fi/artifactory/ext-snapshot-local"}]
                       ["Laughing Panda" {:url "http://maven.laughingpanda.org/maven2"
                                          :snapshots false}]]}
