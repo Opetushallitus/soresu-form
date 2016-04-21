b189513ac48d1384317d53a0043e24c88e50bbda
Eero Anttila <eea@iki.fi>
2016-04-21 10:09:09 +0300

    Rename DB configuration keys to make it more clear; In soresu-form and va-hakija:     :db -> :form-db In va-virkailija:     :db -> :virkailija-db, :hakija-db -> :form-db

--- src/oph/soresu/form/db.clj
+++ src/oph/soresu/form/db.clj
@@ -5,39 +5,39 @@
 
 (defn list-forms []
   (->> {}
-       (exec :db queries/list-forms)))
+       (exec :form-db queries/list-forms)))
 
 (defn get-form [id]
-  (->> (exec :db queries/get-form {:id id})
+  (->> (exec :form-db queries/get-form {:id id})
        first))
 
 (defn update-form! [form]
   ;; NOTE: looks like yesql unwraps sequence parameters, thats why we wrap them one extra time here
   (let [params {:id (:id form) :content (list (:content form)) :rules (list (:rules form))}]
-    (exec-all :db [queries/archive-form! params
+    (exec-all :form-db [queries/archive-form! params
                    queries/update-form! params])))
 
 (defn submission-exists? [form-id submission-id]
   (->> {:form_id form-id :submission_id submission-id}
-       (exec :db queries/submission-exists?)
+       (exec :form-db queries/submission-exists?)
        empty?
        not))
 
 (defn update-submission! [form-id submission-id answers]
   (let [params {:form_id form-id :submission_id submission-id :answers answers}]
-    (exec-all :db [queries/lock-submission params
+    (exec-all :form-db [queries/lock-submission params
                    queries/close-existing-submission! params
                    queries/update-submission<! params])))
 
 (defn create-submission! [form-id answers]
   (->> {:form_id form-id :answers answers}
-       (exec :db queries/create-submission<!)))
+       (exec :form-db queries/create-submission<!)))
 
 (defn get-form-submission [form-id submission-id]
   (->> {:form_id form-id :submission_id submission-id}
-       (exec :db queries/get-form-submission)
+       (exec :form-db queries/get-form-submission)
        first))
 
 (defn get-form-submission-versions [form-id submission-id]
   (->> {:form_id form-id :submission_id submission-id}
-       (exec :db queries/get-form-submission-versions)))
+       (exec :form-db queries/get-form-submission-versions)))
