3c37d6514d85a52d26b07d9f6bc2c86011d331f4
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2017-02-09 15:14:40 +0200

    Update db libraries

--- project.clj
+++ project.clj
@@ -30,10 +30,10 @@
                  [prismatic/schema "0.4.4"]
 
                  ;; SQL + migrations
-                 [yesql "0.5.0"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 [hikari-cp "1.3.0" :exclusions [prismatic/schema]]
-                 [org.flywaydb/flyway-core "3.2.1"]
+                 [yesql "0.5.3"]
+                 [org.postgresql/postgresql "9.4.1212"]
+                 [hikari-cp "1.7.5" :exclusions [prismatic/schema]]
+                 [org.flywaydb/flyway-core "4.0.3"]
 
                  ;; Testing
                  [speclj "3.3.1"]
