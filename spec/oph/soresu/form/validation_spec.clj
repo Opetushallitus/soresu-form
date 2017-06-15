b759631858e646be0e9e573334d53781be488adc
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-06-15 11:59:40 +0300

    Add tests for attachment validation

--- spec/oph/soresu/form/validation_spec.clj
+++ spec/oph/soresu/form/validation_spec.clj
@@ -2,12 +2,31 @@
   (:require [speclj.core :refer :all]
             [oph.soresu.form.validation :refer :all]))
 
+(defn- timestamp-now []
+  (let [millis-now (.getTime (java.util.Date.))]
+    (java.sql.Timestamp. millis-now)))
+
 (defn- answers-for [field value]
   (let [field-id (:id field)
         field-type (:fieldType field)]
     {:value [{:key field-id
               :value value
               :fieldType field-type}]}))
+
+(defn- attachments-for [field filename]
+  (let [field-id (:id field)]
+    {field-id {:id              2
+               :hakemus-id      "2c679cedcb5f62b101dd3a49d053bd7f8ef3e71a538e6c15dff054d9ccab5c1c"
+               :version         0
+               :field-id        (:id field)
+               :file-size       3
+               :content-type    "text/plain"
+               :hakemus-version 5
+               :created-at      (timestamp-now)
+               :filename        filename}}))
+
+(def empty-answers
+  {:value []})
 
 (def email-field
   {:id         "email"
@@ -19,6 +38,12 @@
   {:id         "y-tunnus"
    :fieldClass "formField"
    :fieldType  "finnishBusinessIdField"
+   :required   true})
+
+(def attachment-field
+  {:id         "income-statement"
+   :fieldClass "formField"
+   :fieldType  "namedAttachment"
    :required   true})
 
 (describe "validation"
@@ -68,6 +93,14 @@
 
   (it "validates finnish business id with wrong syntax as invalid"
       (let [result (validate-field (answers-for finnish-business-id-field "165761-0") [] finnish-business-id-field)]
-        (should= {:y-tunnus [{:error "finnishBusinessId"}]} result))))
+        (should= {:y-tunnus [{:error "finnishBusinessId"}]} result)))
+
+  (it "validates attachment"
+      (let [result (validate-field empty-answers (attachments-for attachment-field "file.txt") attachment-field)]
+        (should= {:income-statement []} result)))
+
+  (it "validates missing attachment as required"
+      (let [result (validate-field empty-answers [] attachment-field)]
+        (should= {:income-statement [{:error "required"}]} result))))
 
 (run-specs)
