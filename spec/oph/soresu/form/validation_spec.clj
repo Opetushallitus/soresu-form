2b0644ed0c479e3b12e358eb754eeb10528b6e9a
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-06-15 10:09:54 +0300

    Refactor validation spec to validate field by type

--- spec/oph/soresu/form/validation_spec.clj
+++ spec/oph/soresu/form/validation_spec.clj
@@ -1,72 +1,73 @@
 (ns oph.soresu.form.validation-spec
-  (:require
-    [speclj.core :refer :all]
-    [oph.soresu.form.validation :refer :all]))
+  (:require [speclj.core :refer :all]
+            [oph.soresu.form.validation :refer :all]))
 
-(def finnish-business-id-field
-  {:id "y-tunnus"
-   :fieldClass "formField"
-   :fieldType "finnishBusinessIdField"
-   :required true})
+(defn- answers-for [field value]
+  (let [field-id (:id field)
+        field-type (:fieldType field)]
+    {:value [{:key field-id
+              :value value
+              :fieldType field-type}]}))
 
 (def email-field
-  {:id "email"
+  {:id         "email"
    :fieldClass "formField"
-   :fieldType "emailField"
-   :required true})
+   :fieldType  "emailField"
+   :required   true})
+
+(def finnish-business-id-field
+  {:id         "y-tunnus"
+   :fieldClass "formField"
+   :fieldType  "finnishBusinessIdField"
+   :required   true})
 
 (describe "validation"
-  (it "validates common email"
-      (let [result (validate-email-field email-field "testi@test.org")]
-        (should= [] result)))
+  (it "validates email"
+      (let [result (validate-field (answers-for email-field "testi@test.org") [] email-field)]
+        (should= {:email []} result)))
 
-  (it "validates common email with +part"
-      (let [result (validate-email-field email-field "testi+extra@test.org")]
-        (should= [] result)))
+  (it "validates email with +part"
+      (let [result (validate-field (answers-for email-field "testi+extra@test.org") [] email-field)]
+        (should= {:email []} result)))
 
-  (it "notices invalid email with no @"
-      (let [result (validate-email-field email-field "testitest.org")]
-        (should= [{:error "email"}] result)))
+  (it "validates email with no @ as invalid"
+      (let [result (validate-field (answers-for  email-field "testitest.org") [] email-field)]
+        (should= {:email [{:error "email"}]} result)))
 
-  (it "notices invalid email with only one domain part"
-      (let [result (validate-email-field email-field "testi@test")]
-        (should= [{:error "email"}] result)))
+  (it "validates email with only one domain part as invalid"
+      (let [result (validate-field (answers-for email-field "testi@test") [] email-field)]
+        (should= {:email [{:error "email"}]} result)))
 
-  (it "does not validate empty email"
-      (let [result (validate-email-field email-field "")]
-        (should= [] result)))
+  (it "validates empty email as required"
+      (let [result (validate-field (answers-for email-field "") [] email-field)]
+        (should= {:email [{:error "required"}]} result)))
 
-  (it "requires email if empty given"
-      (let [result (validate-required email-field "")]
-        (should= [{:error "required"}] result)))
+  (it "validates empty finnish-business-id-field as required"
+      (let [result (validate-field (answers-for finnish-business-id-field nil) [] finnish-business-id-field)]
+        (should= {:y-tunnus [{:error "required"}]} result)))
 
-  (it "requires finnish-business-id-field if no given"
-      (let [result (validate-required finnish-business-id-field nil)]
-        (should= [{:error "required"}] result)))
-
-  (it "validates valid finnish business id with checskum 0"
-      (let [result (validate-finnish-business-id-field finnish-business-id-field "0165761-0")]
-        (should= [] result)))
+  (it "validates finnish business with checksum 0"
+      (let [result (validate-field (answers-for finnish-business-id-field "0165761-0") [] finnish-business-id-field)]
+        (should= {:y-tunnus []} result)))
 
   (it "validates common valid finnish business id"
-    (let [result (validate-finnish-business-id-field finnish-business-id-field "1629284-5")]
-      (should= [] result)))
+      (let [result (validate-field (answers-for finnish-business-id-field "1629284-5") [] finnish-business-id-field)]
+        (should= {:y-tunnus []} result)))
 
-  (it "validates valid finnish business id with checskum 0"
-    (let [result (validate-finnish-business-id-field finnish-business-id-field "0165761-0")]
-      (should= [] result)))
+  (it "validates valid finnish business id with checksum 0"
+      (let [result (validate-field (answers-for finnish-business-id-field "0165761-0") [] finnish-business-id-field)]
+        (should= {:y-tunnus []} result)))
 
-  (it "does not validate empty finnish business id"
-      (let [result (validate-finnish-business-id-field finnish-business-id-field "")]
-        (should= [] result)))
+  (it "validates empty finnish business id as required"
+      (let [result (validate-field (answers-for finnish-business-id-field "") [] finnish-business-id-field)]
+        (should= {:y-tunnus [{:error "required"}]} result)))
 
-  (it "notices invalid finnish business id with wrong checskum"
-      (let [result (validate-finnish-business-id-field finnish-business-id-field "1629284-6")]
-        (should= [{:error "finnishBusinessId"}] result)))
+  (it "validates finnish business id with wrong checskum as invalid"
+      (let [result (validate-field (answers-for finnish-business-id-field "1629284-6") [] finnish-business-id-field)]
+        (should= {:y-tunnus [{:error "finnishBusinessId"}]} result)))
 
-  (it "notices invalid finnish business id with wrong syntax"
-      (let [result (validate-finnish-business-id-field finnish-business-id-field "165761-0")]
-        (should= [{:error "finnishBusinessId"}] result)))
-)
+  (it "validates finnish business id with wrong syntax as invalid"
+      (let [result (validate-field (answers-for finnish-business-id-field "165761-0") [] finnish-business-id-field)]
+        (should= {:y-tunnus [{:error "finnishBusinessId"}]} result))))
 
 (run-specs)
