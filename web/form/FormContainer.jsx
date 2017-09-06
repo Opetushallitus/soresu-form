aeaaaf0d334f47e6d8214bcb70db0a4ebc950d8b
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-09-06 21:26:50 +0300

    some efforts with the businessId search..

--- web/form/FormContainer.jsx
+++ web/form/FormContainer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import _ from 'lodash'
+import InputValueStorage from './InputValueStorage'
+import FieldUpdateHandler from './FieldUpdateHandler'
+import axios from 'axios'
 
 import FormPreview from './FormPreview.jsx'
 
 export default class FormContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+      const businessIdField = this.props.state.saveStatus.values.value.filter(value => value.key == "business-id")
+      axios.get("http://localhost:8080/api/organisations/?organisationId=" + businessIdField[0].value).then(({ data })=> {
+        /*Esimerkiksi vain yritän tässä tuota datan nimikenttää päivittää ja tää siis vielä käyttää sitä vanhaa routea (ja toi urlikin tolleen tyhmästi vielä, koska default-api on täällä /api/avustushaku/jotain, niin testauksen vuoksi vain noin)*/
+        console.log(data)
+        const newName = data["name"]
+        InputValueStorage.writeValue(this.props.state.configuration.form, this.props.state.saveStatus.values, FieldUpdateHandler.createFieldUpdate({id: "organisation", fieldClass: "formField", fieldType: "textField", value: newName}))
+     })
+    }
+
   render() {
     const state = this.props.state
     const formContainerClass = this.props.formContainerClass
@@ -18,6 +36,7 @@ export default class FormContainer extends React.Component {
 
     return (
       <section id={containerId}>
+        <a href="#" onClick={this.handleClick}>Hae</a>
         {headerElements}
         {formElement}
       </section>
